aa844091781f13e4f1e244a5b188559f1e03986a
lvxiao <lvxiao@jd.com>
2019-07-16 18:28:32 +0800

    深拷贝和浅拷贝

---
+++ reflect/src/main/java/com/lvxiao/reflect/clone/CloneTest.java
@@ -0,0 +1,65 @@
+package com.lvxiao.reflect.clone;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 浅拷贝---能复制变量，如果对象内还有对象，则只能复制对象的地址
+ *
+ * 深拷贝---能复制变量，也能复制当前对象的 内部对象
+ * @author lvxiao
+ * @date 2019/7/16
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class Student implements Cloneable {
+    private String name;
+    private int age;
+    private Integer test;
+    @Override
+    protected Student clone() {
+        try {
+            return (Student) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class School implements Cloneable{
+    private Student student;
+    private int history;
+    @Override
+    protected School clone() {
+        try {
+            return (School) super.clone();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
+
+public class CloneTest {
+    public static void main(String[] args) {
+        Student student = new Student("lvxiao", 28, 88);
+        Student student1 = student.clone();
+        School school = new School(student, 100);
+        System.out.println(school);
+        System.out.println(student1);
+        student.setName("duanxin");
+        student.setAge(18);
+        student.setTest(18);
+        //值都不改变
+        System.out.println(student1);
+        //内部兑现修改。。其值也跟着修改
+        System.out.println(school);
+
+    }
+}
