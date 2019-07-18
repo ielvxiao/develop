321517ed98b14757f7fc0ac43cc48f23acb6c142
lvxiao <ielvxiao@gmail.com>
2019-07-18 11:32:05 +0800

    代码优化

--- multithread/src/main/java/com/lvxiao/multithread/create/CreateThread.java
+++ multithread/src/main/java/com/lvxiao/multithread/create/CreateThread.java
@@ -8,6 +8,7 @@ import java.util.concurrent.*;
  * 2. 实现Runnable接口
  * 3. 实现Callable接口
  * 4. 通过线程池
+ *
  * @author lvxiao
  * @version V1.0
  * @date 2019-07-18 10:53
@@ -16,6 +17,7 @@ class Base {
     protected static int anInt = 0;
 
 }
+
 class Thread1 extends Thread {
     @Override
     public void run() {
@@ -30,35 +32,42 @@ class Thread2 implements Runnable {
         System.out.println(Base.anInt++);
     }
 }
-class Thread3 implements Callable<Integer>{
+
+class Thread3 implements Callable<Integer> {
 
     @Override
     public Integer call() throws Exception {
         return Base.anInt++;
     }
 }
+
 public class CreateThread {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //method1
         Thread thread1 = new Thread1();
         thread1.setName("extendThread");
+        thread1.start();
         //method2
         Thread thread2 = new Thread(new Thread2());
         thread2.setName("implementRunable");
+        thread2.start();
         //method3
-        FutureTask<Integer> task = new FutureTask<>(new Thread3());
-        try {
-            System.out.println(task.get());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+        Callable<Integer> thread3 = new Thread3();
+        FutureTask<Integer> task = new FutureTask<>(thread3);
+        Thread thread = new Thread(task);
+        thread.start();
+        System.out.println(task.get());
+        task.run();
         //使用线程池Executors创建
         ExecutorService service = Executors.newFixedThreadPool(10);
+        Future<Integer> futureTask = service.submit(new Thread3());
+        System.out.println(futureTask.get());
+        service.shutdown();
         //使用推荐的方式创建线程。
-        ExecutorService threadPool = new ThreadPoolExecutor(1, 5, 30, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<>(1024), Executors.defaultThreadFactory(),new ThreadPoolExecutor.AbortPolicy());
+        ExecutorService threadPool = new ThreadPoolExecutor(1, 5, 30, TimeUnit.MILLISECONDS, new LinkedBlockingDeque<>(1024), Executors.defaultThreadFactory(), new ThreadPoolExecutor.AbortPolicy());
+        System.out.println(threadPool.submit(new Thread3()).get());
+        threadPool.shutdown();
     }
 }
