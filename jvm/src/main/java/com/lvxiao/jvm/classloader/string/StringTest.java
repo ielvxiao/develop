9836eb8b807b8abf7042b8552d9dba6aaa134390
lvxiao <ielvxiao@gmail.com>
2019-11-15 14:34:05 +0800

    String.intern方法学习

--- jvm/src/main/java/com/lvxiao/jvm/classloader/string/StringTest.java
+++ jvm/src/main/java/com/lvxiao/jvm/classloader/string/StringTest.java
@@ -33,19 +33,16 @@ public class StringTest {
             System.out.println("s1 and s5 are same");  // 4.
         }
 
-        String s6 = new String("str") + new String("1");
-        s6.intern();    //放入String pool
-        String s7 = "str1";
+        String s6 = new String("str") + new String("1"); //创建"str"堆对象，"1"堆对象，"str1"对象.在常量池创建"str"与"1"
+        s6.intern();    // 将对象"str1"的引用放入常量池
+        String s7 = "str1"; //存在，取"str1"的引用放入常量池
 
         if (s6 == s7) {
             System.out.println("s6 and s7 are same"); //5
         }
 
-        /*
-        这个case不知道为什么？？？？？TODO
-         */
-        String s8 = new String("a");
-        s8.intern();
+        String s8 = new String("a"); //创建"a"堆对象，与常量池常量
+        s8.intern(); //将
         String s9 = "a";
 
         if (s8 == s9) {
@@ -53,7 +50,45 @@ public class StringTest {
         }
     }
 
+    /*
+     判断这个常量是否存在于常量池。
+  如果存在
+   判断存在内容是引用还是常量，
+    如果是引用，
+     返回引用地址指向堆空间对象，
+    如果是常量，
+     直接返回常量池常量
+  如果不存在，
+   将当前对象引用复制到常量池,并且返回的是当前对象的引用
+     */
+    private static void interTest2() {
+        String first = "Hello"; //创建常量池常量
+        String second = "Hello";
+        System.out.println("first == second:" + (first == second)); //true 两个都是常量池常量
+
+        String third = new String("Hello"); //创建堆上对象，常量池常量存在
+        String fourth = new String("Hello"); //创建堆上对象，常量池常量存在
+        System.out.println("third == fourth:" + (third == fourth)); //false 堆上引用
+        System.out.println("third == fourth.intern()" + (third == fourth.intern()));    //false 堆上引用和常量池常量
+        System.out.println("third.intern() == fourth" + (third.intern() == fourth));    //同上
+        System.out.println("third == fourth" + (third == fourth));  //false 堆上引用
+        System.out.println("third.intern() == fourth.intern()" + (third.intern() == fourth.intern())); //true
+        System.out.println("third.intern() == first" + (third.intern() == first)); // true
+
+        String fifth = new String(new char[]{'H', 'e', 'l', 'l', 'o'}); //创建堆上对象，常量池常量存在
+        String sixth = new String(new char[]{'H', 'e', 'l', 'l', 'o'}); //创建堆上对象，常量池常量存在
+        System.out.println("fifth == fifth.intern()" + (fifth == fifth.intern())); //false 堆上引用和常量池常量
+        System.out.println("sixth == sixth.intern()" + (sixth == sixth.intern()));  //false 堆上引用和常量池常量
+
+        String seven = new String(new char[]{'H', 'e', 'l', 'l', 'o', '2'}); //创建堆上对象与常量池常量
+        String eight = new String(new char[]{'H', 'e', 'l', 'l', 'o', '2'}); //创建堆上对象 常量池常量存在
+        System.out.println("seven == seven.intern()" + (seven == seven.intern()));  //常量池引用，引用是堆上对象地址
+        System.out.println("eight == eight.intern()" + (eight == eight.intern()));  // eight.intern()是seven
+        System.out.println("seven == eight.intern()" + (seven == eight.intern()));
+
+    }
+
     public static void main(String[] args) {
-        internTest();
+        interTest2();
     }
 }
