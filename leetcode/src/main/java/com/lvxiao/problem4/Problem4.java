ae72f0052b5a8ccfe4075a6acb581c68c8ee316a
lvxiao <ielvxiao@gmail.com>
2019-11-27 17:28:59 +0800

    4. Median of Two Sorted Arrays

--- leetcode/src/main/java/com/lvxiao/problem4/Problem4.java
+++ leetcode/src/main/java/com/lvxiao/problem4/Problem4.java
@@ -1,27 +1,49 @@
 package com.lvxiao.problem4;
 
-import java.util.HashMap;
-import java.util.Map;
-
 /**
  * @author lvxiao
  * @version V1.0
  * @date 2019-07-09 21:50
  */
 public class Problem4 {
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        if (nums1.length < nums2.length) {
-            int[] tmp = nums1;
-            nums1 = nums2;
-            nums2 = tmp;
-        }
-        int mid = (nums1.length + nums2.length - 1) / 2;
-        int length1 = nums1.length;
-        int length2 = nums2.length;
-        if (nums1[(length1 - 1) / 2] < nums2[(length2 - 1) / 2]) {
 
-        }
-        return 0;
+    /**
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int left = (m + n + 1) / 2;
+        int right = (m + n + 2) / 2;
+        return (findKth(nums1, 0, nums2, 0, left) + findKth(nums1, 0, nums2, 0, right)) / 2.0;
     }
 
+    /**
+     * 寻找第k个数
+     *
+     * @param nums1  数组1
+     * @param index1 数组1查询开始坐标
+     * @param nums2  数组2
+     * @param index2 数组2查询开始坐标
+     * @param k      坐标
+     * @return int
+     */
+    public int findKth(int[] nums1, int index1, int[] nums2, int index2, int k) {
+        if (index1 >= nums1.length) return nums2[index2 + k - 1];//nums1为空数组
+        if (index2 >= nums2.length) return nums1[index1 + k - 1];//nums2为空数组
+        if (k == 1) {
+            return Math.min(nums1[index1], nums2[index2]);
+        }
+        int midVal1 = (index1 + k / 2 - 1 < nums1.length) ? nums1[index1 + k / 2 - 1] : Integer.MAX_VALUE;
+        int midVal2 = (index2 + k / 2 - 1 < nums2.length) ? nums2[index2 + k / 2 - 1] : Integer.MAX_VALUE;
+        //midVal1<midVal2，可能是midVal1<midVal2或者midVal2长度太短，则，此时，需要将保持将index1坐标右移动k/2，此时从1,2坐标寻找第k - k / 2小就行
+        if (midVal1 < midVal2) {
+            return findKth(nums1, index1 + k / 2, nums2, index2, k - k / 2);
+        } else {
+            return findKth(nums1, index1, nums2, index2 + k / 2, k - k / 2);
+        }
+    }
 }
+
