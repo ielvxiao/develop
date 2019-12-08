940d51b4cd17f0e65468de42407626ea4f5a7124
lvxiao <ielvxiao@gmail.com>
2019-12-08 13:57:50 +0800

    47. Permutations II

---
+++ leetcode/src/main/java/com/lvxiao/problem47/Problem47.java
@@ -0,0 +1,35 @@
+package com.lvxiao.problem47;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author lvxiao
+ * @version V1.0
+ * @date 2019/12/7 5:00 下午
+ */
+public class Problem47 {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, new boolean[nums.length]);
+        return list;
+    }
+
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums, boolean [] used){
+        if(tempList.size() == nums.length){
+            list.add(new ArrayList<>(tempList));
+        } else{
+            for(int i = 0; i < nums.length; i++){
+                if(used[i] || i > 0 && nums[i] == nums[i-1] && !used[i - 1]) continue;
+                used[i] = true;
+                tempList.add(nums[i]);
+                backtrack(list, tempList, nums, used);
+                used[i] = false;
+                tempList.remove(tempList.size() - 1);
+            }
+        }
+    }
+
+}
