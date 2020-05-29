c9f317f06195ccc78eb757b32f63dffad3513429
lvxiao <ielvxiao@gmail.com>
2020-05-29 17:48:54 +0800

    打家劫舍非dp算法

--- leetcode/src/main/java/com/lvxiao/problem198/Problem198.java
+++ leetcode/src/main/java/com/lvxiao/problem198/Problem198.java
@@ -15,7 +15,20 @@ public class Problem198 {
         return dp[dp.length - 1];
     }
 
+    public int robNoDp(int[] nums) {
+        int len = nums.length;
+        if(len ==0)  return 0;
+        if(len ==1)  return nums[0];
+        int preTwo = nums[0];
+        int preOne = nums[1];
+        for (int i = 2; i < len; i++) {
+            int tmp = preOne;
+            preOne = Math.max(preTwo + nums[i], preOne);
+            preTwo = Math.max(tmp, preTwo);
+        }
+        return Math.max(preOne,preTwo);
+    }
     public static void main(String[] args) {
-        System.out.println(new Problem198().rob(new int[]{3,1,1,2}));
+        System.out.println(new Problem198().robNoDp(new int[]{2,1,1,2}));
     }
 }
