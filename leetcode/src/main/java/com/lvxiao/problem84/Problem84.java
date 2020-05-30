18729c4dd0a0c65ea86420735e062bc3d3a518b5
lvxiao <ielvxiao@gmail.com>
2020-05-30 17:13:01 +0800

    Problem84 单调栈的应用

---
+++ leetcode/src/main/java/com/lvxiao/problem84/Problem84.java
@@ -0,0 +1,55 @@
+package com.lvxiao.problem84;
+
+import java.util.Stack;
+
+/**
+ * @author lvxiao
+ * @date 2020/5/30
+ */
+public class Problem84 {
+    public int largestRectangleArea(int[] heights) {
+        int area = 0, n = heights.length;
+        // 遍历每个柱子，以当前柱子的高度作为矩形的高 h，
+        // 从当前柱子向左右遍历，找到矩形的宽度 w。
+        for (int i = 0; i < n; i++) {
+            int w = 1, h = heights[i], j = i;
+            while (--j >= 0 && heights[j] >= h) {
+                w++;
+            }
+            j = i;
+            while (++j < n && heights[j] >= h) {
+                w++;
+            }
+            area = Math.max(area, w * h);
+        }
+
+        return area;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Problem84().largestRectangleAreaStack(new int[]{2,2,2,2,2}));
+    }
+    public int largestRectangleAreaStack(int[] heights) {
+        // 这里为了代码简便，在柱体数组的头和尾加了两个高度为 0 的柱体。
+        int[] tmp = new int[heights.length + 2];
+        System.arraycopy(heights, 0, tmp, 1, heights.length);
+
+        Stack<Integer> stack = new Stack<>();
+        int area = 0;
+        for (int i = 0; i < tmp.length; i++) {
+            // 对栈中柱体来说，栈中的下一个柱体就是其「左边第一个小于自身的柱体」；
+            // 若当前柱体 i 的高度小于栈顶柱体的高度，说明 i 是栈顶柱体的「右边第一个小于栈顶柱体的柱体」。
+            // 因此以栈顶柱体为高的矩形的左右宽度边界就确定了，可以计算面积🌶️ ～
+            while (!stack.isEmpty() && tmp[i] < tmp[stack.peek()]) {
+                int h = tmp[stack.pop()];
+                area = Math.max(area, (i - stack.peek() - 1) * h);
+            }
+            stack.push(i);
+        }
+
+        return area;
+    }
+
+
+
+}
