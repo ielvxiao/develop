afe83c141e86620d5f0f10b62727b52e0c4eefdf
lvxiao <ielvxiao@gmail.com>
2020-06-11 14:33:47 +0800

    putIfAbsent vs computeIfAbsent

--- computerscience/src/main/java/com/lvxiao/algorithm/java8/MapTest.java
+++ computerscience/src/main/java/com/lvxiao/algorithm/java8/MapTest.java
@@ -8,6 +8,14 @@ import java.util.*;
  */
 public class MapTest {
 
+    /**对比
+     * @see Map#putIfAbsent(Object, Object)
+     * 最大的区别是compute方法传入的不是具体的value，而是一个function。
+     * 所以，当传入的value需要复杂的计算时，使用compute**方法是比较合适的。
+     * @param map
+     * @param key
+     * @param value
+     */
     private static void computeIf(Map<Integer, Integer> map, int key, int value) {
         map.put(key, value);
         //不管存在不存在某值，最后都保存
