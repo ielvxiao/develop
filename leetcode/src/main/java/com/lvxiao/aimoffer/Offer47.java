a36dac29de84c855bade015c8ee5a258ac354865
lvxiao <ielvxiao@gmail.com>
2020-06-19 16:57:56 +0800

    常规题目

---
+++ leetcode/src/main/java/com/lvxiao/aimoffer/Offer47.java
@@ -0,0 +1,24 @@
+package com.lvxiao.aimoffer;
+
+/**
+ * @author lvxiao
+ * @date 2020/6/19
+ */
+public class Offer47 {
+    public int maxValue(int[][] grid) {
+        if (grid.length==0) return 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (i == 0 && j == 0) {
+                } else if (i == 0) {
+                    grid[i][j] += grid[i][j - 1];
+                } else if (j == 0) {
+                    grid[i][j] += grid[i-1][j];
+                } else {
+                    grid[i][j] += Math.max(grid[i - 1][j], grid[i][j - 1]);
+                }
+            }
+        }
+        return grid[grid.length - 1][grid[0].length - 1];
+    }
+}
