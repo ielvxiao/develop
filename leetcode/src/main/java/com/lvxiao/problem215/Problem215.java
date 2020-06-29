5b8fd98794a104024ac3303b74c0e89f558edccc
lvxiao <ielvxiao@gmail.com>
2020-06-29 10:16:17 +0800

    快速排序法

--- leetcode/src/main/java/com/lvxiao/problem215/Problem215.java
+++ leetcode/src/main/java/com/lvxiao/problem215/Problem215.java
@@ -1,6 +1,9 @@
 package com.lvxiao.problem215;
 
 
+import java.util.Iterator;
+import java.util.PriorityQueue;
+import java.util.Queue;
 import java.util.Random;
 
 /**
@@ -11,105 +14,36 @@ import java.util.Random;
  * @date 2019/10/31 5:26 下午
  */
 public class Problem215 {
-    /**
-     * 解答1：需要改进
-     * @param nums
-     * @param k
-     * @return
-     */
     public int findKthLargest(int[] nums, int k) {
-        for (int i = 0; i < k; i++) {
-            heapInsert(nums, i);
-        }
-        return nums[k - 1];
+        return help(nums, k, 0, nums.length - 1);
     }
 
-
-    //构造大根堆（通过新插入的数上升）
-    public static void heapInsert(int[] arr, int index) {
-        for (int i = index; i < arr.length; i++) {
-            int start = i;
-            int father = (start + index - 1) / 2;
-            while (arr[start] > arr[father]) {
-                swap(arr, father, start);
-                start = father;
-                father = (start + index - 1) / 2;
+    private int help(int[] nums, int k, int left, int right) {
+        if (left >= right) {
+            return nums[left];
+        }
+        int pave = nums[left];
+        int i = left,j=right;
+        while (i < j) {
+            while (i<j&&nums[j]<pave) j--;
+            if (i < j) {
+                nums[i] = nums[j];
+            }
+            while (i<j&&nums[i]>=pave) i++;
+            if (i < j) {
+                nums[j] = nums[i];
             }
         }
-    }
-
-
-    //交换数组中两个元素的值
-    public static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    int [] nums;
-
-    public void swap(int a, int b) {
-        int tmp = this.nums[a];
-        this.nums[a] = this.nums[b];
-        this.nums[b] = tmp;
-    }
-
-
-    public int partition(int left, int right, int pivot_index) {
-        int pivot = this.nums[pivot_index];
-        // 1. move pivot to end
-        swap(pivot_index, right);
-        int store_index = left;
-
-        // 2. move all smaller elements to the left
-        for (int i = left; i <= right; i++) {
-            if (this.nums[i] < pivot) {
-                swap(store_index, i);
-                store_index++;
-            }
+        if (i+1 == k) {
+            return pave;
         }
-
-        // 3. move pivot to its final place
-        swap(store_index, right);
-
-        return store_index;
-    }
-
-    public int quickselect(int left, int right, int k_smallest) {
-    /*
-    Returns the k-th smallest element of list within left..right.
-    */
-
-        if (left == right) // If the list contains only one element,
-        {
-            return this.nums[left];  // return that element
+        nums[i] = pave;
+        if (i+1 < k) {
+            return help(nums, k, i+1, right);
+        } else {
+            return help(nums, k, left, i-1);
         }
-
-        // select a random pivot_index
-        Random random_num = new Random();
-        int pivot_index = left + random_num.nextInt(right - left);
-
-        pivot_index = partition(left, right, pivot_index);
-
-        // the pivot is on (N - k)th smallest position
-        if (k_smallest == pivot_index) {
-            return this.nums[k_smallest];
-        }
-            // go left side
-        else if (k_smallest < pivot_index) {
-            return quickselect(left, pivot_index - 1, k_smallest);
-        }
-        // go right side
-        return quickselect(pivot_index + 1, right, k_smallest);
     }
-
-    public int findKthLargestQuickSelect(int[] nums, int k) {
-        this.nums = nums;
-        int size = nums.length;
-        // kth largest is (N - k)th smallest
-        return quickselect(0, size - 1, size - k);
-    }
-
     public static void main(String[] args) {
         System.out.println(new Problem215().findKthLargest(new int[]{3, 2, 3, 4, 2, 4, 5, 5,1, 6}, 5));
     }
