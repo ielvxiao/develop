09ee8e451a4fc565f2b0341de01bf02dd65fddb1
lvxiao <ielvxiao@gmail.com>
2020-07-28 14:10:08 +0800

    interrupted方法会返回当前线程的中断状态，而且还会把当前的中断状态修改为false

--- multithread/src/main/java/com/lvxiao/multithread/interrupt/InterruptTest.java
+++ multithread/src/main/java/com/lvxiao/multithread/interrupt/InterruptTest.java
@@ -2,6 +2,7 @@ package com.lvxiao.multithread.interrupt;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 /**
  * @author lvxiao
@@ -25,11 +26,41 @@ public class InterruptTest {
         }
     }
 
-    public static void main(String[] args) {
-        ExecutorService service = Executors.newFixedThreadPool(10);
-        for (int i = 0; i < 10; i++) {
-            service.submit(() -> InterruptTest.test1());
-//            Thread.currentThread().interrupt();
+    /**
+     * false
+     * false
+     * false
+     * true
+     * false
+     * false
+     * false
+     * 很多false包围了一个true，也就是interrupted方法判断了中断，然后擦除了标记，后边再判断都是false了
+     * @throws InterruptedException
+     */
+    public static void testInterrupted() throws InterruptedException {
+        Thread thread = new Thread(() -> {
+            while (true) {
+                System.out.println(Thread.interrupted());
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+        TimeUnit.MILLISECONDS.sleep(2);
+        thread.interrupt();
+    }
+
+    public static void main(String[] args)  {
+        System.out.println("Main thread is interrupted before ? "+Thread.interrupted()); //返回false,因为没有被中断
+        Thread.currentThread().interrupt();
+        System.out.println("Main thread is interrupted? "+Thread.currentThread().isInterrupted()); //因为中断所以返回true
+
+//        System.out.println("Main thread is interrupted after ? "+Thread.interrupted()); //加上这句不会进入异常咯？？ 可能是还没进到异常里边，状态被清除了
+
+        System.out.println("Main thread is interrupted? "+Thread.currentThread().isInterrupted()); //如果加上注释行，则会返回false，否则是true。
+        try {
+            TimeUnit.MILLISECONDS.sleep(1);
+        } catch (InterruptedException e) {
+            System.out.println("中断咯~~");
         }
     }
 }
