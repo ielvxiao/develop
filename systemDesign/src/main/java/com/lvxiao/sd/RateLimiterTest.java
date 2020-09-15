ca3b83e22f00e03e2909b1b8805aa8ef3405b2a6
lvxiao <ielvxiao@gmail.com>
2020-09-15 15:38:47 +0800

    RateLimiterTest

---
+++ systemDesign/src/main/java/com/lvxiao/sd/RateLimiterTest.java
@@ -0,0 +1,77 @@
+package com.lvxiao.sd;
+
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+import com.google.common.util.concurrent.RateLimiter;
+
+/**
+ * 输出结果：
+ * I have acquired a permit~~😆
+ * time is 2020-09-15 15:13:31 080
+ * failed to acquire 😢
+ * time is 2020-09-15 15:13:31 188
+ * failed to acquire 😢
+ * time is 2020-09-15 15:13:31 291
+ * failed to acquire 😢
+ * time is 2020-09-15 15:13:31 393
+ * I have acquired a permit~~😆
+ * time is 2020-09-15 15:13:31 499
+ * failed to acquire 😢
+ * time is 2020-09-15 15:13:31 600
+ * failed to acquire 😢
+ * time is 2020-09-15 15:13:31 701
+ * failed to acquire 😢
+ * time is 2020-09-15 15:13:31 802
+ * failed to acquire 😢
+ * time is 2020-09-15 15:13:31 905
+ * I have acquired a permit~~😆
+ * time is 2020-09-15 15:13:32 010
+ * failed to acquire 😢
+ * time is 2020-09-15 15:13:32 111
+ * failed to acquire 😢
+ * time is 2020-09-15 15:13:32 216
+ * failed to acquire 😢
+ * time is 2020-09-15 15:13:32 316
+ * failed to acquire 😢
+ * time is 2020-09-15 15:13:32 417
+ * I have acquired a permit~~😆
+ * time is 2020-09-15 15:13:32 519
+ *
+ * 可以看出来，RateLimiter除了会对每秒的访问频次做限制，还会控制两个请求之间的时间。
+ * @author lvxiao <lvxiao@kuaishou.com>
+ * Created on 2020-09-15
+ */
+public class RateLimiterTest {
+
+    final static RateLimiter LIMITER = RateLimiter.create(2.0);
+    //时间转字符串格式化
+    final static DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss SSS");
+    static class ThreadTest extends Thread {
+        @Override
+        public void run() {
+            for (int i = 0; i < 100; i++){
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                if (LIMITER.tryAcquire()) {
+                    System.out.println("I'm "+Thread.currentThread().getName()+"I have acquired a permit~~😆");
+                } else {
+                    System.out.println("I'm "+Thread.currentThread().getName()+"failed to acquire 😢");
+                }
+                String dateTime = LocalDateTime.now(ZoneOffset.of("+8")).format(formatter);
+                System.out.println("time is " + dateTime);
+            }
+        }
+    }
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1 = new ThreadTest();
+        Thread t2 = new ThreadTest();
+        t1.start();
+        t2.start();
+
+    }
+}
