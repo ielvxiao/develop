9f6309ee9942202111bd96990879f16b53b51b68
lvxiao <lvxiao@kuaishou.com>
2020-12-01 20:21:26 +0800

    正则匹配

--- computerscience/src/main/java/com/lvxiao/algorithm/regexp/RegExpTest.java
+++ computerscience/src/main/java/com/lvxiao/algorithm/regexp/RegExpTest.java
@@ -14,5 +14,10 @@ public class RegExpTest {
         System.out.println(Pattern.compile(digitExp1).matcher("12").matches());
         String digitExp2 = "^\\d{3}$"; //匹配3位数字
         System.out.println(Pattern.compile(digitExp2).matcher("1234").matches());
+        String digitExp3 = "^\\d{3,}$"; //匹配最少3位数字的数字
+        System.out.println(Pattern.compile(digitExp3).matcher("12").matches());
+        String digitExp4 = "^[1]\\d{10}$"; //1开头后边10个数字
+        System.out.println(Pattern.compile(digitExp4).matcher("10123456789").matches());
+
     }
 }
