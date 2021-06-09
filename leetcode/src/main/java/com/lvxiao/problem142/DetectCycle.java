4234c7c3a4b757f17af06a56a7c7e2b5381f032a
lvxiao <ielvxiao@gmail.com>
2021-06-09 23:45:06 +0800

    add

--- leetcode/src/main/java/com/lvxiao/problem142/DetectCycle.java
+++ leetcode/src/main/java/com/lvxiao/problem142/DetectCycle.java
@@ -15,18 +15,35 @@ public class DetectCycle {
         }
         ListNode slow = head;
         ListNode fast = head;
-        ListNode sslow = null;
-        ListNode tmp = null;
-        while (fast != null && fast.next != null) {
+        boolean isCycle = false;
+        //看一下是否有环
+        while (fast != null || fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
             if (slow == fast) {
-                tmp = slow;
+                isCycle = true;
+                break;
             }
         }
-        if (tmp != null) {
-
+        /**
+         假设a是环前边的长度，b为环的长度,x为fast与slow相遇的地方离相交处的距离。
+         fast 走了 a + nb + x步
+         slow 走了 a + mb + x步
+         fast = 2low = 2a + 2mb + 2x
+         a = (n-2m)b-x
+         加入再走a步，则
+         fast+a=3a + 2 mb + 2x = a + 2(n-m)b则刚好走到圈的起始处
+         */
+        if (isCycle) {
+            //有环的话从头开始一步一步来
+            slow = head;
+            while (slow != fast) {
+                slow = slow.next;
+                fast = fast.next;
+            }
+            return slow;
+        } else {
+            return null;
         }
-        return null;
     }
 }
